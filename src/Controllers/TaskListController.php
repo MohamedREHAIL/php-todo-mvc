ac32fac7ee029823621c2ca3d1dff2b833a3138e
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2022-11-09 10:25:03 +0100

    commit
    remise du projet

--- src/Controllers/TaskListController.php
+++ src/Controllers/TaskListController.php
@@ -2,9 +2,33 @@
 class TaskListController extends AbstractController {
   
   public function render () : void {
+     $orderBy=null;
+     $search=null;
+      $order=null;
+    //  $search = $_GET['search'];
+      if(isset($_GET['search'])) {
+          $search = $_GET['search'];
+
+      }
+      //$orderBy=$_GET['order-by'];
+      if(isset($_GET['order-by'])){
+          $orderBy=$_GET['order-by'];
+      }
+
+
+
+
+
+
+
+
+
    echo get_template( __PROJECT_ROOT__ . "/Views/list.php", [
-     'tasks' => $this->taskService->list()['tasks']
+     'tasks' => $this->taskService->list(["search"=>$search,"orderBy"=>$orderBy])['tasks']
+
    ] );
+
+
   }
   
 }
